88d30a0805995c50b2176102e3ad3eeff3e9d992
Dennis Eichhorn <spl1nnes.com@googlemail.com>
2023-10-17 00:13:58 +0000

    fix jsonSerialize function call

--- tests/Models/NullContactElementTest.php
+++ tests/Models/NullContactElementTest.php
@@ -47,6 +47,6 @@ final class NullContactElementTest extends \PHPUnit\Framework\TestCase
     public function testJsonSerialize() : void
     {
         $null = new NullContactElement(2);
-        self::assertEquals(['id' => 2], $null);
+        self::assertEquals(['id' => 2], $null->jsonSerialize());
     }
 }
--- tests/Models/NullProfileTest.php
+++ tests/Models/NullProfileTest.php
@@ -47,6 +47,6 @@ final class NullProfileTest extends \PHPUnit\Framework\TestCase
     public function testJsonSerialize() : void
     {
         $null = new NullProfile(2);
-        self::assertEquals(['id' => 2], $null);
+        self::assertEquals(['id' => 2], $null->jsonSerialize());
     }
 }
