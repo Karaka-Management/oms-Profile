7ddc3499c6483d74e35494c36855ae4717e99f66
Dennis Eichhorn <spl1nes.com@googlemail.com>
2024-05-21 00:09:15 +0200

    fix version and bugs

--- Admin/Uninstaller.php
+++ Admin/Uninstaller.php
@@ -6,7 +6,7 @@
  *
  * @package   Modules\Profile\Admin
  * @copyright Dennis Eichhorn
- * @license   OMS License 2.0
+ * @license   OMS License 2.2
  * @version   1.0.0
  * @link      https://jingga.app
  */
@@ -20,7 +20,7 @@ use phpOMS\Module\UninstallerAbstract;
  * Uninstaller class.
  *
  * @package Modules\Profile\Admin
- * @license OMS License 2.0
+ * @license OMS License 2.2
  * @link    https://jingga.app
  * @since   1.0.0
  */
--- Controller/BackendController.php
+++ Controller/BackendController.php
@@ -6,7 +6,7 @@
  *
  * @package   Modules\Profile
  * @copyright Dennis Eichhorn
- * @license   OMS License 2.0
+ * @license   OMS License 2.2
  * @version   1.0.0
  * @link      https://jingga.app
  */
@@ -30,7 +30,7 @@ use phpOMS\Views\View;
  * Profile class.
  *
  * @package Modules\Profile
- * @license OMS License 2.0
+ * @license OMS License 2.2
  * @link    https://jingga.app
  * @since   1.0.0
  * @codeCoverageIgnore
